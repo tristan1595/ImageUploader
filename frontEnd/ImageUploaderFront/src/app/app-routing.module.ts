c3a0985a61e12c4781fd753e0aa3fc93bc3d7b76
tristan <tris@g>
2021-07-23 10:50:19 +0200

    nav bar

--- frontEnd/ImageUploaderFront/src/app/app-routing.module.ts
+++ frontEnd/ImageUploaderFront/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DragAndDropComponent } from './drag-and-drop/drag-and-drop.component';
+import { GaleryComponent } from './galery/galery.component';
+import { LoadingComponent } from './loading/loading.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'dragAndDrop', component: DragAndDropComponent},
+  {path: 'loading', component: LoadingComponent},
+  {path: 'galery', component: GaleryComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontEnd/ImageUploaderFront/src/app/app.module.ts
+++ frontEnd/ImageUploaderFront/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { DragAndDropComponent } from './drag-and-drop/drag-and-drop.component';
+import { LoadingComponent } from './loading/loading.component';
+import { GaleryComponent } from './galery/galery.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DragAndDropComponent,
+    LoadingComponent,
+    GaleryComponent
   ],
   imports: [
     BrowserModule,
